19d2c72774336bc7f4d1d540460c852e2bdd9132
dsnesia <109241727+dsnesia@users.noreply.github.com>
2023-04-18 16:15:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,30 +8,30 @@ const {
 
 //sebagian panel gk support opena ai jadi mungkin apikey lu bakalan di blokir
 //klo misalkan pada saat memakai fitur ai, terus dapat email dari open si, itu berarti website tempat lu install bot gk support ai
-global.OpenAIKey = "YOUR_KEY" //login to https://platform.openai.com/account/api-keys to get apikey
+global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
-   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+   alfa: 'https://api-v3.dsnesia.my.id', //apabila link api eror, segera laporkan ke owner
 }
 
-//buy apikey premium 0887435047326
-// Free apikey (silahkan login terus ganti V12 dgn apikey lu)
+// beli apikey premium pc wa: 0887435047326
+// beli apikey premium pc wa: 0887435047326
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://api-v3.dsnesia.my.id': 'APIKEY', // 👉 login https://api.zeeoneofc.my.id to get apikey, tutor https://youtu.be/THsiM3I7sLI
 }
 global.sewabotnya = `
-edit sewabot di file settings.js line 24
+PAKE NYANYA😂
 `
 // setting 
 global.firtsChat = true //send firts message to new user
 global.autoread = true // auto read message
 global.antidelete = false // anti delete
-global.antiviewonce = true // anti view once
+global.antiviewonce = false // anti view once
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call, jika true maka org yg nelpon bot auto di blokir
-global.userRegister = false
-global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
+global.userRegister = true
+global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 global.autolevelup = true
 global.autowelcome = true //auto welcome msg all group
 global.autogoodbye = true //auto goodbye msg all group
@@ -46,35 +46,34 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Satirbot-Mdོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄ Lolicon ×፝֟͜×" //nama kalian
+global.myweb = "https://ruby927.blogspot.com/" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@user-qd3yo9sd2u" //bebas asal jan hapus
+global.myweb2 = "https://ruby927.blogspot.com/" //bebas
+global.email = "kepohpedia@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium_fitur = ["ai", "openai", "chatgpt"] //tambahin aja sendiri, soalnya gk semua org mau fiturnya premium jadi gw buat kyk gini aja, biar bisa custom fitur mana aja yang bakal di jadiin premium
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['6288220195739'] //premium user
+global.owner = ["6288220195739", "6285786530730"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6288220195739" // nomor wa kalian
+global.ownernomerr = "+6288220195739" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© ' + botname //sticker wm ubah
-global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
+global.packname = '© Satirbot-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh Lolicon' //sticker wm ganti nama kalian
 global.sessionName = 'session'
-global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
+//type menu for menu
 //type menu for menu
 global.typemenu = 'templateImage' //
 //type menu for downloadmenu, animemenu, etc
-global.typemenu2 = 'image' //
+global.typemenu2 = 'templateImage' //
 global.multiplier = 69
 global.limitawal = {
-   premium: "Infinity", //unlimited
-   free: 100
+   premium: "10000",
+   free: 1000
 }
 global.buttonDefault = [{
    urlButton: {
@@ -120,7 +119,7 @@ global.buttonDefault2 = [{
 }, {
    quickReplyButton: {
       displayText: 'Owner 👤',
-      id: '.owner'
+      id: 'owner'
    }
 }, {
    quickReplyButton: {
